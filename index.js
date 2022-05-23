e778f05f1eb1a2c6b140b9ae15daf47bf8994170
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-05-23 12:48:49 -0300

    fix add acess

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(json())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://saia-de-filo-ecommerce.vercel.app/');
+    res.header("Access-Control-Allow-Origin", 'https://driven-cracy.herokuapp.com/');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
