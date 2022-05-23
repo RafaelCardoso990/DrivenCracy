4e053b00d1a5a2cfb2e908a7231a633cc12b6ca8
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-05-23 12:47:02 -0300

    add acess

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ dotenv.config();
 
 const app = express();
 app.use(json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://saia-de-filo-ecommerce.vercel.app/');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(cors())
 
 app.use(pollRouter)
